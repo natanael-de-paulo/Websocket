78a6a52ed89eaacaf9ea6d48b5f19a836201e8e7
Natanael <natanaeldepaulo123@gmail.com>
2022-06-25 20:23:25 -0300

    Initial commit

---
+++ src/http.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import path from 'path'
+import { createServer } from "http"
+import { Server } from "socket.io"
+
+const app = express()
+const server = createServer(app)
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+const io =  new Server(server);
+
+io.on("connection", (socket) => console.log("socket", socket.id));
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello Websocket",
+  });
+});
+
+
+export {server, io}
