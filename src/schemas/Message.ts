c6fe92330866944bf9d2ad596ded9dd20fcab3f2
Natanael <natanaeldepaulo123@gmail.com>
2022-06-26 22:35:14 -0300

    created schema message

---
+++ src/schemas/Message.ts
@@ -0,0 +1,28 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+type Message = Document & {
+  to: string;
+  text: string;
+  roomId: string;
+  createdAt: Date;
+}
+
+const MessageSchema = new Schema({
+  to: {
+    type: Schema.Types.ObjectId,
+    ref: "Users"
+  },
+  text: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  roomId:{
+    type: String,
+    ref: "ChatRoom"
+  }
+});
+
+const Message = mongoose.model<Message>("Messages",MessageSchema )
+
+export { Message }
