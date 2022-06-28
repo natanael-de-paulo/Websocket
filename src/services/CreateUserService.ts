6238dffc58ba38be29b77aabce3b0cdad990b2b3
Natanael <natanaeldepaulo123@gmail.com>
2022-06-27 22:47:22 -0300

    Create services createUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { User } from '../schemas/User
+
+interface CreateUserDTO {
+  email: string;
+  socketId: string;
+  avatar: string;
+  name: string;
+}
+
+class CreateUserService {
+  async execute({ email, socketId, avatar, name }: CreateUserDTO){
+    const userAlreadyExists = await User.findOne({
+      email
+    }).exec()
+
+    if(userAlreadyExists) {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: userAlreadyExists._id  
+        },
+        {
+          $set: { socketId, avatar, name },
+        }
+      )
+      return user
+    } else{
+        const user = await User.create({
+          email,
+          socketId, 
+          avatar, 
+          name 
+        })
+        return user
+    }
+  }
+}
+
+export { CreateUserService }
