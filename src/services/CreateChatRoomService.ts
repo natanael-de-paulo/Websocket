b18e4afb7397cdbd88455d304c08d08ea4385de9
Natanael <natanaeldepaulo123@gmail.com>
2022-07-05 23:25:33 -0300

    feat: acionado funcionalidade para lembrar id da sala entre dois usuarios.

--- src/services/CreateChatRoomService.ts
+++ src/services/CreateChatRoomService.ts
@@ -3,7 +3,7 @@ import { ChatRoom } from "../schemas/ChatRoom";
 
 @injectable()
 class CreateChatRoomService {
-  async execute(idUsers : string[]){
+  async execute(idUsers : string[] | any){
     const room = await ChatRoom.create({
       idUsers,
     }) 
