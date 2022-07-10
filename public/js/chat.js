ba9b793dd6d374e53dbe1c734c6a9fbba22ef041
Natanael <natanaeldepaulo123@gmail.com>
2022-07-09 22:29:37 -0300

    fix: correção de duplicação de mensagens ao clicar no usuario

--- public/js/chat.js
+++ public/js/chat.js
@@ -90,6 +90,8 @@ function addUser(user) {
 }
 
 document.getElementById('users_list').addEventListener('click', e => {
+  document.getElementById('message_user').innerHTML = ''
+
   if (e.target && e.target.matches('li.user_name_list')) {
     const idUser = e.target.getAttribute('idUser')
 
